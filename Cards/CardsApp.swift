058eb545d51bd019bd44e821d2ee335243fc9455
[tes] <tesneeme11@gmail.com>
2024-10-17 02:33:04 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Tes Essa on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
